91b64c07fe0c2b8511e90bd55a499075983bd4eb
sone <sthaya@gmail.com>
2022-06-14 21:50:42 -0700

    person class

---
+++ ClassesAndObjects/Person.cs
@@ -0,0 +1,35 @@
+﻿// primitive datatypes
+// int, string, char, bool
+
+// object oriented programming - creating your own datatype
+// class - blueprint
+
+// Single responsibility principle
+
+
+
+public class Person
+{
+    // properties when public
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    // fields
+    private double _salary { get; set; }
+
+    public void setSalary(double salary)
+    {
+        _salary = salary;
+    }
+
+    public double getSalary()
+    {
+        return _salary;
+    }
+
+    public string getFullName()
+    {
+        return FirstName + " " + LastName;
+    }
+ }
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Person person = new Person();
+
+Console.WriteLine("Enter first name: ");
+person.FirstName = Console.ReadLine();
+
+Console.WriteLine("Enter last name: ");
+person.LastName = Console.ReadLine();
+
+Console.WriteLine("Enter age: ");
+person.Age = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Enter Salary: ");
+int salary = Convert.ToInt32(Console.ReadLine());
+
+person.setSalary(salary);
+
+Console.WriteLine("First Name is: " + person.FirstName);
+Console.WriteLine("Last Name is: " + person.LastName);
+Console.WriteLine("Full name is: " + person.getFullName());
+
+Console.WriteLine("Age is: " + person.Age);
+Console.WriteLine("Salary is: " + person.getSalary());
